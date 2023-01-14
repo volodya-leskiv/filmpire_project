6a81f3aab8bb4c776c9eb94e4a4b6e0da4b244df
Volodymyr <lesksv2000@gmail.com>
2023-01-14 13:02:15 +0200

    folder structure

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+import { Actors, Movies, NavBar, Profile, MovieInformation } from '.';
+
+const App = () => (
+  <div>
+    <CssBaseline />
+    <NavBar />
+    <main>
+      <Switch>
+        <Route exact path="/movie/:id">
+          <MovieInformation />
+        </Route>
+        <Route exact path="/actors/:id">
+          <Actors />
+        </Route>
+        <Route exact path="/">
+          <Movies />
+        </Route>
+        <Route exact path="/profile/:id">
+          <Profile />
+        </Route>
+      </Switch>
+    </main>
+  </div>
+);
+
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  console.log('Navbar');
+  return (
+    <div>
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;
